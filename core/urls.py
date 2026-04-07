c411a275cd5ed716b85ce0ff85b08bb921338e02
Yuanlim <zuyuanlim@gmail.com>
2026-04-08 00:49:31 +0800

    Tried creating one skill-based endpoint. Skill serializer updated, removed test file from apis.

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from apps.accounts.apis.skill_api import SkillSearchAndInsert
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/skill/searchAndInsert/', SkillSearchAndInsert.as_view(), name="skill_search_andI_insert")
 ]
